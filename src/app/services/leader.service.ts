629f5238779e875be5cd3ed777cca75d831c91cb
Daniel <daniel.paschin@gmail.com>
2020-04-03 06:45:34 +0300

    Angular Assignment 22

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -1,15 +1,24 @@
 import { Injectable } from '@angular/core';
+
+import { Observable } from 'rxjs';
+import { delay } from 'rxjs/operators';
+import { of as observableOf } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { baseURL } from '../shared/baseurl';
+
+
 import { Leader } from '../shared/leader';
-import { LEADERS } from '../shared/leaders';
+// import { LEADERS } from '../shared/leaders';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LeaderService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  getLeaders(): Leader[] {
-    return LEADERS;
+  getLeaders(): Observable<Leader[]> {
+    // return LEADERS;
+    return this.http.get<Leader[]>(baseURL + 'leaders');
   }
 }
